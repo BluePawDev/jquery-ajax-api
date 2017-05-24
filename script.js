1a6fd571b1a5b4f8edf6c52cc5e6534ba33bb595
BluePawDev <jaruby@me.com>
2017-05-24 16:28:11 -0500

    Initial commit

--- script.js
+++ script.js
@@ -13,11 +13,25 @@ $(document).on('click', '#serachNow', function(){
     searchURL += '&api_key=dc6zaTOxFJmzC';
     // make ajax call to Giphy to retrieve JSON
     $.ajax({
+      type: 'GET', // default of .ajax()...do not need to explicitly define(?)
       url: searchURL,
       dataType: 'JSON',
-      success: function(data){
+      success: function(response){ // 'data' argument (or whatever you want to call it) will always be the response from the server
         // successfully hit API
-        console.log('successful API hit:', data);
+        console.log('successful API hit:', response);
+        console.log('array?', response.data);
+        console.log('0th result:', response.data[0]);
+        console.log('0th result.images:', response.data[0].images);
+        console.log('0th result.images.downsized:', response.data[0].images.downsized);
+        console.log('0th result.images.downsized.url:', response.data[0].images.downsized.url);
+        // display on document
+        var outputDiv = $('#outputDiv');
+        outputDiv.empty();
+        outputDiv.append('<img src="' + response.data[0].images.downsized.url +'">');
+        console.log(response.data[0].images.downsized.url);
+
+
+
       } // end success
     }); // end ajax
   } // end of actual input
