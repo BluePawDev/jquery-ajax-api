881170415539080e8ac3c4e06e8140eec9780f6c
BluePawDev <jaruby@me.com>
2017-05-24 10:58:04 -0500

    Add if-else

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
 // console.log('js/jq sourced!');
 
 $(document).on('click', '#serachNow', function(){
-  // assemble search URL
-  // redefine search url
-  var searchURL = 'https://api.giphy.com/v1/gifs/search?q=';
-  // append user input into URL
-  searchURL += $('#searchIn').val();
-  searchURL += '&api_key=dc6zaTOxFJmzC';
-  // make ajax call to Giphy to retrieve JSON
-  $.ajax({
-    url: searchURL,
-    dataType: 'JSON',
-    success: function(data){
-      // successfully hit API
-      console.log('successful API hit:', data);
-    } // end success
-  }); // end ajax
+  if ($('#searchIn').val() === '') {
+    alert('nothing to search for?!?');
+  }
+  else {
+    // assemble search URL
+    // redefine search url
+    var searchURL = 'https://api.giphy.com/v1/gifs/search?q=';
+    // append user input into URL
+    searchURL += $('#searchIn').val();
+    searchURL += '&api_key=dc6zaTOxFJmzC';
+    // make ajax call to Giphy to retrieve JSON
+    $.ajax({
+      url: searchURL,
+      dataType: 'JSON',
+      success: function(data){
+        // successfully hit API
+        console.log('successful API hit:', data);
+      } // end success
+    }); // end ajax
+  } // end of actual input
 }); // end click on search button
